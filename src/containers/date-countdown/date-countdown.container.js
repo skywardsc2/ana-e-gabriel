b66407e11e46da5b320ea340c48e07c0ff04b5e4
Rodrigo Andrade <rodrigo.andrade@sflabs.com.br>
2021-09-07 13:13:05 -0300

    Add date-countdown container

---
+++ src/containers/date-countdown/date-countdown.container.js
@@ -0,0 +1,77 @@
+import { Box, Grid, Heading, Text } from '@chakra-ui/react'
+import { StaticImage } from 'gatsby-plugin-image'
+import React, { useEffect, useState } from 'react'
+
+const DateCountdownContainer = () => {
+  const calculateTimeLeft = () => {
+    let difference = +new Date('01/22/2022') - +new Date()
+    let timeLeft = {}
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60)
+      }
+    }
+
+    return timeLeft
+  }
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft())
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft())
+    }, 1000)
+    // Clear timeout if the component is unmounted
+    return () => clearTimeout(timer)
+  })
+
+  return (
+    <Grid
+      justifyContent='center'
+      justifyItems='center'
+      alignContent='center'
+      width='100%'
+      my='8'
+      px='2'
+      rowGap='8'
+    >
+      <Box pos='relative' overflow='hidden'>
+        <StaticImage
+          src='../../img/splashes/Date.png'
+          alt=''
+          style={{ width: '768px' }}
+        />
+        <Heading
+          as='h2'
+          pos='absolute'
+          top='50%'
+          left='50%'
+          width='100%'
+          textAlign='center'
+          transform='translate(-50%, -50%)'
+          fontFamily='Rawengulk'
+          fontWeight='normal'
+          color='lightShade'
+        >
+          {'22 de janeiro de 2022'}
+        </Heading>
+      </Box>
+      <Heading
+        as='h2'
+        width='100%'
+        textAlign='center'
+        fontFamily='Rawengulk'
+        fontWeight='normal'
+        color='primary'
+      >
+        {`${timeLeft.days} dias, ${timeLeft.hours} horas, ${timeLeft.minutes} minutos, ${timeLeft.seconds} segundos`}
+      </Heading>
+    </Grid>
+  )
+}
+
+export default DateCountdownContainer
